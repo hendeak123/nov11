14f4108a177d679526a40ac59e47954350da5ca9
hendeak123 <alyssa.henderson@svu.edu>
2020-11-11 10:48:01 -0500

    Initial commit

---
+++ src/com/company/IFunWithQueue.java
@@ -0,0 +1,8 @@
+package com.company;
+
+import java.util.Queue;
+
+public interface IFunWithQueue {
+    void initializeQueue();
+    void iterateQueue(Queue<Integer> numbers);
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+
+    public static void main(String[] args) {
+        IFunWithQueue funWithQueue = new FunWithQueue();
+        funWithQueue.initializeQueue();
+    }
+}
