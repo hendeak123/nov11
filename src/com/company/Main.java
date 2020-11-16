9882f8523a772ef6a9609cfa57a6342685780016
hendeak123 <alyssa.henderson@svu.edu>
2020-11-16 10:11:29 -0500

    added new Pet constructor

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,13 +7,14 @@ public class Main {
 
     public static void main(String[] args) {
         IFunWithQueue<Pet> funWithQueue = new FunWithQueue<Pet>();
-        Pet[] numbers = new Pet[6];
+        Pet[] numbers = new Pet[7];
         numbers[0] = new Pet("Joe", 5);
         numbers[1] = new Pet("George", 35);
         numbers[2] = new Pet("Eddy", 100);
         numbers[3] = new Pet("Jackson", 4);
         numbers[4] = new Pet("Zeus", 42);
         numbers[5] = new Pet("Poseidon", 11);
+        numbers[6] = new Pet("Hades", 21);
         funWithQueue.initializeQueue(numbers);
     }
 }
